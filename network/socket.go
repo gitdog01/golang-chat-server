1a35fb82357414f76a1b30e8d8349611bd2f4bde
mononomo <hostdog01@naver.com>
2024-04-14 20:54:04 +0900

    type definition

--- network/socket.go
+++ network/socket.go
@@ -1,5 +1,26 @@
 package network
 
-type Socket struct {
-	
+import "golang.org/x/net/websocket"
+
+type Message struct {
+	Name    string
+	Message string
+	Time    int64
+}
+
+type Room struct {
+	Forward chan *Message // 수신하는 메시지를 보관하는 값
+	// 들어오는 메세지를 다른 클라이언트에게 전송합니다.
+
+	Join  chan *Client
+	Leave chan *Client
+
+	Clients map[*Client]bool
+}
+
+type Client struct {
+	Send   chan Message
+	Room   *Room
+	Name   string
+	Socket websocket.Conn
 }
